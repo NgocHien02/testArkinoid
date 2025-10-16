9baa1767f045fccdcb0233afee18dbfb98aff932
Nguyen Le Duy Nhan <nguyenleduynhan06@gmail.com>
2025-10-16 12:01:52 +0700

    add map loading from file

--- src/main/java/org/example/gamearkanoid/MainApp.java
+++ src/main/java/org/example/gamearkanoid/MainApp.java
@@ -3,14 +3,15 @@ package org.example.gamearkanoid;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import org.example.gamearkanoid.controller.GameController;
-import org.example.gamearkanoid.model.Ball;
-import org.example.gamearkanoid.model.BlockBrick;
-import org.example.gamearkanoid.model.Paddle;
+import org.example.gamearkanoid.model.*;
 
 public class MainApp extends Application {
     private Scene scene;
+    private GameMap gameMap = new GameMap("/maps/lv1.txt");
     private BlockBrick blockObject = new BlockBrick();
     private Ball ballObject;
     private Paddle paddleObject = new Paddle(400, 400);
@@ -21,14 +22,14 @@ public class MainApp extends Application {
     public void start(Stage stage) throws Exception {
         Group root = new Group();
 
-        blockObject.addBrick(4, 6);
-        for (int i = 0; i < blockObject.getBlock().size(); i++) {
-            root.getChildren().add(blockObject.getBlock().get(i).getBrick());
+        scene = new Scene(root, 800, 800);
+
+        blockObject.addBrick(gameMap.getLayout());
+        for (Brick brick : blockObject.getBlock()) {
+            root.getChildren().add(brick.getBrickImageView());
         }
 
-        scene = new Scene(root, 800, 800);
         controller = new GameController(scene, root);
-
         ballObject = new Ball(scene.getWidth() / 2 + 50, scene.getHeight() - 150 , 10);
         paddleObject = new Paddle(scene.getWidth() / 2 , scene.getHeight() - 100);
         root.getChildren().addAll(ballObject.getBall(), paddleObject.getPaddle());
--- src/main/java/org/example/gamearkanoid/model/BlockBrick.java
+++ src/main/java/org/example/gamearkanoid/model/BlockBrick.java
@@ -1,25 +1,55 @@
 package org.example.gamearkanoid.model;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BlockBrick {
     private ArrayList<Brick> block = new ArrayList<>();
     private double dx = 10;
     private double dy = 10;
 
-    public void addBrick(int row, int column) {
-        double x = 0;
+    private Map<Integer, Image> brickImages = new HashMap<>();
+
+    public BlockBrick() {
+        loadBrickImages();
+    }
+
+    private void loadBrickImages() {
+        try {
+            Image brick1 = new Image(getClass().getResourceAsStream("/images/123.png"), Brick.DEFAULT_HEIGHT_BRICK, Brick.DEFAULT_WIDTH_BRICK, true, false);
+            brickImages.put(1, brick1);
+
+            Image brick2 = new Image(getClass().getResourceAsStream("/images/1234.png"), Brick.DEFAULT_HEIGHT_BRICK, Brick.DEFAULT_WIDTH_BRICK, true, false);
+            brickImages.put(2, brick2);
+
+        } catch (Exception e) {
+            System.err.println("Error loading brick images:");
+            e.printStackTrace();
+        }
+    }
+
+    public void addBrick(int[][] layout) {
         double y = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (j == 0) {
-                    x = 0;
+        for (int i = 0; i < layout.length; i++) {
+            double x = 0;
+            for (int j = 0; j < layout[i].length; j++) {
+                int brickType = layout[i][j];
+
+                if (brickType > 0) {
+                    Image brickImage = brickImages.get(brickType);
+                    if (brickImage != null) {
+                        Brick newBrick = new Brick(x, y, brickImage);
+                        block.add(newBrick);
+                    } else {
+                        System.err.println("No image found for brick type");
+                    }
                 }
-                else {
-                    x = x + Brick.DEFAULT_WIDTH_BRICK + dx;
-                }
-                Brick newBrick = new Brick(x, y);
-                block.add(newBrick);
+                x = x + Brick.DEFAULT_WIDTH_BRICK + dx;
             }
             y = y + Brick.DEFAULT_HEIGHT_BRICK + dy;
         }
